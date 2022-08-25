8cef9c3cd13da9b971b106772c450492cf3cd99c
Nelson Sandoval <techednelson@gmail.com>
2022-08-25 16:44:18 +0300

    initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = '0x91ea267Dd0f4C523C580B309E2413c62AffDE05D';
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,43 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+const { CRYPTODEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  // Now deploy the CryptoDevsDAO contract
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+    {
+      // This assumes your account has at least 0.1 ETH in it's account
+      // Change this value as you want
+      value: ethers.utils.parseEther("0.1"),
+    }
+  );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
